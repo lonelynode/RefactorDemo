24ad8eec7ab3de825d86bb626f6735414ccfceef
martin.zhou@oocl.com <martin.zhou@oocl.com>
2018-08-24 14:46:31 +0800

    # create a CustomerTest.java file which could test the function of customer

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -1,4 +1,16 @@
 package rentalstore;
 
+import form_template_method.Customer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+    private form_template_method.Customer customer = new Customer("Jerry");
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_no_rental() {
+        String statement = customer.statement();
+        assertEquals("Rental Record for Jerry\nAmount owed is 0.0\nYou earned 0 frequent renter points", statement);
+    }
 }
